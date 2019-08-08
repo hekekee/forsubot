302fce937294812aa5cbdfe939e0004baaf68f12
Hekekee <heikkistark@gmail.com>
2019-08-08 09:50:33 +0000

    Cleaned output a little

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json'); // config.json in this case contains the client token
+const { token, users_id } = require('./config.json'); // config.json in this case contains the client token
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
 
-// Listen to messages
+// Listen to messages and add reactions to specific user's messages
 client.on('message', message => {
-	if (message.author.id === '231110335245975552') {
+	if (message.author.id === users_id) {
 		message.react('🍆');
 	}
 });
